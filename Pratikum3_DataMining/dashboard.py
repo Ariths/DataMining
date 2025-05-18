157c3a402a1d6499af623db5e2847f1bf12fd4ab
Ariths <92672790+Ariths@users.noreply.github.com>
2025-05-18 18:50:11 +0700

    Update dashboard.py

--- Pratikum3_DataMining/dashboard.py
+++ Pratikum3_DataMining/dashboard.py
@@ -1,4 +1,6 @@
 import streamlit as st
+streamlit run dashboard.py
+df = pd.read_csv("./data/covid_19_indonesia_time_series_all.csv")
 
 st.set_page_config(page_title="COVID-19 Clustering Dashboard", layout="wide")
 st.title("Clustering Dashboard COVID-19")
